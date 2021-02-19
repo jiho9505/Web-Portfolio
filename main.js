5d32ebc03a6da75348ebd444ed718bd0d1c869ad
jiho9505 <sun_days@naver.com>
2021-02-19 12:02:39 +0900

    Added intersection observer api

--- main.js
+++ main.js
@@ -85,3 +85,47 @@ actButton4.addEventListener("click", () => {
 actButton5.addEventListener("click", () => {
     actPhoto[4].classList.toggle('view')
 });
+
+
+const sectionIds = [
+    '#home',,
+    '#skills',
+    '#work',
+    '#activitys',
+    '#contact',
+  ];
+  const sections = sectionIds.map(id => document.querySelector(id));
+  const navItems = sectionIds.map(id =>
+    document.querySelector(`[data-link="${id}"]`)
+  );
+  
+  let selectedNavIndex = 0;
+  let selectedNavItem = navItems[0];
+  function selectNavItem(selected) {
+    selectedNavItem.classList.remove('active');
+    selectedNavItem = selected;
+    selectedNavItem.classList.add('active');
+  }
+  
+  const observerOptions = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.3,
+  };
+  
+  const observerCallback = (entries, observer) => {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting && entry.intersectionRatio > 0) {
+        const index = sectionIds.indexOf(`#${entry.target.id}`);
+        // 스크롤링이 아래로 되어서 페이지가 올라옴
+        if (entry.boundingClientRect.y < 0) {
+          selectedNavIndex = index + 1;
+        } else {
+          selectedNavIndex = index - 1;
+        }
+      }
+    });
+  };
+  
+  const observer = new IntersectionObserver(observerCallback, observerOptions);
+  sections.forEach(section => observer.observe(section));
